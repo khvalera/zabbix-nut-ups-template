d4750899943318a046d8b92e00c4ab3e2947f425
khvalera <khvalera@ukr.net>
2024-07-03 15:06:49 +0300

    v.0.0.2

--- zabbix-agent2/ups-data.sh
+++ zabbix-agent2/ups-data.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+#====================================================
+# https://github.com/khvalera/zabbix-nut-ups-template
+#====================================================
+
 ups=$1
 key=$2
 if [ "$key" == "ups.state.mode" ]; then
    state=`/bin/upsc $ups ups.status 2>&1 | grep -v SSL`
-   if [ ! -z `echo $state | grep "OL"` ]; then
+   if [[ ! -z `echo $state | grep "OL"` ]]; then
       echo 1;
-   elif [ ! -z `echo $state | grep "OB"` ]; then
+   elif [[ ! -z `echo $state | grep "OB"` ]]; then
       echo 2;
-   elif [ ! -z `echo $state | grep "BYPASS"` ]; then
+   elif [[ ! -z `echo $state | grep "BYPASS"` ]]; then
       echo 3;
-   elif [ ! -z `echo $state | grep "OFF"` ]; then
+   elif [[ ! -z `echo $state | grep "OFF"` ]]; then
       echo 4;
-   elif [ ! -z `echo $state | grep "FSD"` ]; then
+   elif [[ ! -z `echo $state | grep "FSD"` ]]; then
       echo 5;
    else
       echo 0;
@@ -20,9 +24,9 @@ if [ "$key" == "ups.state.mode" ]; then
 # Voltage stabilization
 elif [ "$key" == "ups.state.stab" ]; then
    state=`/bin/upsc $ups ups.status 2>&1 | grep -v SSL`
-   if [ ! -z `echo $state | grep "TRIM"` ]; then
+   if [[ ! -z `echo $state | grep "TRIM"` ]]; then
       echo 1;
-   elif [ ! -z `echo $state | grep "BOOST"` ]; then
+   elif [[ ! -z `echo $state | grep "BOOST"` ]]; then
       echo 2;
    else
       echo 0;
@@ -30,9 +34,9 @@ elif [ "$key" == "ups.state.stab" ]; then
 # The battery is charging or discharging
 elif [ "$key" == "battery.charg_discharg" ]; then
    state=`/bin/upsc $ups ups.status 2>&1 | grep -v SSL`
-   if [ ! -z `echo $state | grep "CHRG"` ]; then
+   if [[ ! -z `echo $state | grep "CHRG"` ]]; then
       echo 1;
-   elif [ ! -z `echo $state | grep "DISCHRG"` ]; then
+   elif [[ ! -z `echo $state | grep "DISCHRG"` ]]; then
       echo 2;
    else
       echo 0;
@@ -40,16 +44,16 @@ elif [ "$key" == "battery.charg_discharg" ]; then
 # Battery low or high
 elif [ "$key" == "battery.low_high" ]; then
    state=`/bin/upsc $ups ups.status 2>&1 | grep -v SSL`
-   if [ ! -z `echo $state | grep "LB"` ]; then
+   if [[ ! -z `echo $state | grep "LB"` ]]; then
       echo 1;
-   elif [ ! -z `echo $state | grep "HB"` ]; then
+   elif [[ ! -z `echo $state | grep "HB"` ]]; then
       echo 2;
    else
       echo 0;
    fi
 # The battery needs to be replaced
 elif [ "$key" == "battery.replaced" ]; then
-   if [ ! -z `echo $state | grep "RB"` ]; then
+   if [[ ! -z `echo $state | grep "RB"` ]]; then
       echo 1;
    else
       echo 0;
